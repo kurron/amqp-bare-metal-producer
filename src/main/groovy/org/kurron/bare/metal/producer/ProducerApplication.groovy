087715ef4519c700a374465f60be38137fda1815
Ron Kurr <kurr@kurron.org>
2016-12-23 12:40:27 -0500

    first commit

---
+++ src/main/groovy/org/kurron/bare/metal/producer/ProducerApplication.groovy
@@ -0,0 +1,12 @@
+package org.kurron.bare.metal.producer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProducerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ProducerApplication, args
+	}
+}
