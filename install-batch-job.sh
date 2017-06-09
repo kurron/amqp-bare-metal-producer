dd8b266ac5bad480979402a44dd7b1fdb18dd3c2
Ron Kurr <kurr@kurron.org>
2017-06-09 10:49:40 -0400

    Initial stab at installation

---
+++ install-batch-job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INSTALL="kubectl create --filename k8s-batch-job.yml"
+echo ${INSTALL}
+${INSTALL}
+
+STATUS="kubectl get jobs --show-all"
+echo ${STATUS}
+${STATUS}
+
+# use kubectl logs <job name> to see how it did
